b1ca47a12bf5560ff6c4aab3e79bc69d8eebafbd
Felipe Marques de Araujo <felipe_marques_araujo@hotmail.com>
2022-08-12 18:38:31 -0300

    Estrutura inicial de Realizando Deploy na Nuvem de um conjunto de API’s desenvolvida em Spring Boot

--- js/DOM/assets/js/script.js
+++ js/DOM/assets/js/script.js
@@ -1,32 +1,8 @@
-function changeMode(){
-    changeClasses();
-    changeText();
-}
+var urls = new Array();
+urls[0] = "https://www.google.com.br/";
+urls[1] = "https://ge.globo.com/";
+urls[2] = "https://www.meutimao.com.br/";
 
-function changeClasses(){
-    button.classList.toggle(darkModeClass);
-    h1.classList.toggle(darkModeClass);
-    body.classList.toggle(darkModeClass);
-    footer.classList.toggle(darkModeClass);
-}
+window.location = urls[random];
 
-function changeText(){
-    const lightMode = 'Light Mode';
-    const darkMode = 'Dark Mode';
-
-    if(body.classList.contains(darkModeClass)){
-        button.innerHTML = lightMode;
-        h1.innerHTML = darkMode + " ON";
-        return;
-    }
-    button.innerHTML = darkMode;
-    h1.innerHTML = lightMode + " ON";
-}
-
-const darkModeClass = 'dark-mode';
-const button = document.getElementById('mode-selector');
-const h1 = document.getElementById('page-title');
-const body = document.getElementsByTagName('body')[0];
-const footer = document.getElementsByTagName('footer')[0];
-
-button.addEventListener('click',changeMode);
+++counterVal
